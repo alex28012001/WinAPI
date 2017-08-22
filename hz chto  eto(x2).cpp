93cce762e400f668337eb6fadc4fae36bb420be1
alex28012001 <star4enko.aleksey2015@yandex.ru>
2017-08-22 20:13:45 +0300

    Create hz chto  eto(x2).cpp

---
+++ hz chto  eto(x2).cpp
@@ -0,0 +1,54 @@
+#include<windows.h>
+#include"resource.h"
+#include<string>
+BOOL CALLBACK DlgProc(HWND, UINT, WPARAM, LPARAM);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevlnstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	HWND hWnd = CreateDialog(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, DlgProc);
+	ShowWindow(hWnd, 1);
+
+	MSG msg;
+	while (GetMessage(&msg, 0, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return msg.wParam;
+}
+
+BOOL CALLBACK DlgProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
+{
+	wchar_t buf[20];
+	static HWND hBUTOK;
+	switch (iMsg)
+	{
+	case WM_COMMAND:
+	{
+		if (LOWORD(wParam) == IDOK)
+		{	
+			GetWindowText(hBUTOK, buf,20);
+			if (std::wstring(buf) ==  L"OK") 
+				SetWindowText(hBUTOK, L"O_O");
+			else if (std::wstring(buf) ==  L"O_O") 
+				SetWindowText(hBUTOK, L"$_$");
+			else if (std::wstring(buf)== L"$_$")
+				SetWindowText(hBUTOK, L"O_O");
+		}
+
+	}break;
+	
+	case WM_INITDIALOG:
+		hBUTOK = GetDlgItem(hwnd,IDOK);
+		break;
+
+	
+
+
+	case WM_CLOSE:
+		DestroyWindow(hwnd);
+		PostQuitMessage(0);
+	return true;
+	}
+	return false;
+}
